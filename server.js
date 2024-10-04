44c427a88adc2818a4255d6a48b1246ca5fc8288
defycab <love.bellinder@gmail.com>
2024-10-04 13:22:59 +0200

    add script to run server. Fix missing port variable
    Co-authored-by: Adrienn <apozsgai97@gmail.com>
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Welcome to our chat app');
 });
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is runing on port ${port}`);
 });
