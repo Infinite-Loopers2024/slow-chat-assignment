b425df142a53220cf0b3b9a87dc71b061c3cf60a
defycab <love.bellinder@gmail.com>
2024-10-04 13:50:49 +0200

    problem with import express const app in jest test
    Co-authored-by: Adrienn <apozsgai97@gmail.com>
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to our chat app');
 });
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is runing on port ${port}`);
 });
+
+module.exports = app;
--- server.test.js
+++ server.test.js
@@ -1,8 +1,11 @@
-import request from 'supertest';
+const request = require('supertest');
+const app = require('./server.js');
 
-describe( "Connection to server", () => {
-    it("Get/ should return Welcome to our chat app", async () => {
-        const response = await request(app).get('/')
-        expect(response.body).toEqual('Welcome to our chat app')
-    })
-})
+describe('Connection to server', () => {
+  it('Get/ should return Welcome to our chat app', async () => {
+    const response = await request(app)
+      .get('/')
+      .expect(response.body)
+      .toEqual('Welcome to our chat app');
+  });
+});
